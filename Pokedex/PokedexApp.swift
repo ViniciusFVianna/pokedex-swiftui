7c5952e4fd0f067b18fb0894ee5458b7aa4692ed
ViniciusFVianna <vinicius-durap-16@hotmail.com>
2024-12-12 23:29:57 -0300

    First commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Vinícius Vianna on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
